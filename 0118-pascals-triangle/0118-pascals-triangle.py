4099aeb09b4bba69e41fd23f64e9ebe3d7d43d7f
ManmohanBoyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-06-14 15:51:10 -0400

    Time: 18 ms (42.01%), Space: 11.3 MB (99.37%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,26 +1,26 @@
-class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        n=numRows
+class Solution(object):
+    def generate(self, numRows):
         a=[]
-        if n>=1:
+        if numRows>=1:
             col=[]
             col.append(1)
             a.append(col)
-        if n>=2:
+        if numRows>=2:
             col=[]
             col.append(1)
             col.append(1)
             a.append(col)
         i=3
-        c=3
-        while(i<=n):
+        while(i<=numRows):
             col=[]
             col.append(1)
-            d=len(a[i-2])
-            for j in range(d-1):
+            for j in range(len(a[i-2])-1):
                 col.append(a[i-2][j]+a[i-2][j+1])
             col.append(1)
             a.append(col)
             i=i+1
         return a
+        
+
+
         
