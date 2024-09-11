77de8f988cb423684d45ccca573add81ddbe4020
ManmohanBoyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-09-10 23:38:59 -0400

    Time: 40 ms (22.13%), Space: 16.5 MB (26.96%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        n=start^goal
+        c=0
+        while n!=0:
+            n=n&n-1
+            c+=1
+        return c
+        
