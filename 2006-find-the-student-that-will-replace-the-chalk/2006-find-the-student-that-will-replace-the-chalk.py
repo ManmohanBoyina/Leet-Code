1b3bc33691b2e817bc02da3d3b5742452c7574f2
ManmohanBoyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-09-11 22:55:19 -0400

    Time: 623 ms (5.54%), Space: 30.7 MB (23.24%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.py
@@ -2,8 +2,8 @@ class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         total_chalk = sum(chalk)
         k %= total_chalk
-        for i, amount in enumerate(chalk):
-            if k < amount:
-                return i
-            k -= amount
-        return -1
+        i = 0
+        while k >= chalk[i]:
+            k -= chalk[i]
+            i += 1
+        return i
