71568c779767e67a9ba7e5adc74d3fde91cf8031
ManmohanBoyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-09-12 21:01:42 -0400

    Time: 314 ms (20.79%), Space: 31.3 MB (56.55%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.py
@@ -2,10 +2,10 @@ from typing import List
 
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        prefix_xor = [0] * (len(arr) + 1)
-        for i in range(1, len(arr) + 1):
-            prefix_xor[i] = prefix_xor[i - 1] ^ arr[i - 1]
-        result = []
-        for l, r in queries:
-            result.append(prefix_xor[r + 1] ^ prefix_xor[l])
-        return result
+        prefix_xor=[0]*(len(arr)+1)
+        for i in range(1,len(arr)+1):
+            prefix_xor[i]=prefix_xor[i-1]^arr[i-1]
+        res=[]
+        for l,r in queries:
+            res.append(prefix_xor[r+1]^prefix_xor[l])
+        return res
