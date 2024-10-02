b488f9c8a1d6d529cdbbe54c712070e45b53ebff
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-10-01 23:50:29 -0400

    Time: 473 ms (5.03%), Space: 59.8 MB (5.11%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -1,18 +1,10 @@
 from numpy import *
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        at=arr.copy()
-        at.sort()
-        a=defaultdict(int)
-        c=1
-        for i in range(len(arr)):
-            if at[i] not in a:
-                a[at[i]]=c
-                c+=1
-        for i in range(len(arr)):
-            print(arr[i])
-            arr[i]=a[arr[i]]
-            
+        at=sorted(set(arr))
+        c={ele:rank+1 for rank,ele in enumerate(at)}
+        print(c)
+        arr=[c[ele] for ele in arr]
         return arr
 
         
