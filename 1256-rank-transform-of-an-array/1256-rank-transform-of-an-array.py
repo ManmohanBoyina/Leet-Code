77d982b6877ddbf7f4e0fb50e99307d31ca8685d
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-10-01 23:50:48 -0400

    Time: 460 ms (5.03%), Space: 59.5 MB (5.11%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -3,7 +3,6 @@ class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
         at=sorted(set(arr))
         c={ele:rank+1 for rank,ele in enumerate(at)}
-        print(c)
         arr=[c[ele] for ele in arr]
         return arr
 
