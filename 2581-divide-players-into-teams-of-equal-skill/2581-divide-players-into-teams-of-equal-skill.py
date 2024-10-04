12980df0dec6d46c62e7a23880a83b2197906cc2
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-10-04 15:14:47 -0400

    Time: 424 ms (22.3%), Space: 30.6 MB (6.9%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -1,6 +1,7 @@
 class Solution:
     def dividePlayers(self, skills: List[int]) -> int:
         a=[]
+        sum=0
         if len(skills)==2:
             return skills[0]*skills[1]
         skills.sort()
@@ -8,14 +9,11 @@ class Solution:
         while i<j:
             if skills[i]+skills[j]==skills[i+1]+skills[j-1]:
                 a.append((skills[i],skills[j]))
+                sum+=(skills[i]*skills[j])
                 i+=1
                 j-=1
             else:
                 return -1
-        sum=0
-        for i in range(len(a)):
-            sum+=(a[i][0]*a[i][1])
         return sum
-
 
         
