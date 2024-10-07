2cfd22f90982e646384794bf1df305235a2390e4
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-10-07 15:37:03 -0400

    Time: 33 ms (95.97%), Space: 16.6 MB (56.78%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -1,7 +1,14 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        # get number of set bits in the binary representation of (k - 1)
-        flips = bin(k-1).count('1')
-        # final character is determined by how many flips (bit counts) have occurred
-        # we start with 'a' and move ahead in the alphabet by the number of flips
-        return chr(ord('a') + flips)
+        s = "a"
+        def fstr(s, k):
+            if len(s) >= k:
+                return s[k-1]
+            n = len(s)
+            for i in range(n):
+                s += chr(ord(s[i]) + 1)
+            return fstr(s, k)
+        return fstr(s, k)
+
+        
+        
