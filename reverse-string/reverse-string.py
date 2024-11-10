cf11a88e79777dd594c6c259b6dc72841d60f154
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-11-10 16:04:02 -0500

    Time: 3 ms, Memory: 22.2 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,20 +1,11 @@
-from typing import List
-
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        def helper(left: int, right: int):
-            # Base case: if the pointers cross each other, stop recursion
-            if left >= right:
-                return
-            # Swap the elements at the left and right pointers
-            s[left], s[right] = s[right], s[left]
-            # Recursive call with updated pointers
-            helper(left + 1, right - 1)
-        
-        # Start the recursion with the two ends of the list
-        helper(0, len(s) - 1)
-
-        
-
-
+        left,right=0,len(s)-1
+        while left<=right:
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+        return s
+            
+            
         
