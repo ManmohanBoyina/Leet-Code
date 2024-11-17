ce6e3193c0d2ddee37608b72637f00f11dcb2c9c
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-11-16 19:18:17 -0500

    Time: 4 ms (49.4%), Space: 18.7 MB (83.89%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,7 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        a={}
-        for i in nums:
-            if i in a:
-                a[i]+=1
-            else:
-                a[i]=1
-        for i in a:
-            if a[i]==1:
-                return int(i)
+class Solution(object):
+    def singleNumber(self, nums):
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
         
