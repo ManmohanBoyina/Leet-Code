d54f02f0a356e84c5f0e38eeb5d52d441d042701
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-11-19 17:05:44 -0500

    Time: 0 ms, Memory: 17 MB - LeetHub

--- largest-unique-number/largest-unique-number.py
+++ largest-unique-number/largest-unique-number.py
@@ -2,10 +2,10 @@ from collections import Counter
 class Solution:
     def largestUniqueNumber(self, nums: List[int]) -> int:
         a=Counter(nums)
-        maxi=float('-inf')
+        maxi=-1
         for i in a.keys():
             if a[i]==1:
                 maxi=max(maxi,i)
-        return -1 if maxi==float('-inf') else maxi
+        return maxi
             
         
