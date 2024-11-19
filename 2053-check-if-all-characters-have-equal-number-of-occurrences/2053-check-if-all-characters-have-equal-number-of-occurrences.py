250e163fab3a3e61b23aedd9010e819048b1fdda
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2024-11-18 21:04:54 -0500

    Time: 1 ms (74.62%), Space: 16.6 MB (41.61%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -6,9 +6,6 @@ class Solution:
                 a[i]+=1
             else:
                 a[i]=1
-        temp=a[s[0]]
-        for i in a:
-            if a[i]!=temp:
-                return False
-        return True
+        frequencies = a.values()
+        return len(set(frequencies)) == 1
         
