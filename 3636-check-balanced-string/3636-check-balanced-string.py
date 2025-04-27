7d35277f0a10051f83a64b0d9d0ed3db62aeddae
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2025-04-27 10:56:07 -0400

    Time: 0 ms (100%), Space: 17.8 MB (67.77%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.py
+++ 3636-check-balanced-string/3636-check-balanced-string.py
@@ -1,14 +1,9 @@
 class Solution:
     def isBalanced(self, num: str) -> bool:
-        sum1,sum2=0,0
-        count=0
-        while int(num)!=0:
-            temp=int(num)%10
-            if count%2==0:
-                sum1+=temp
-            else:
-                sum2+=temp
-            num=str(int(num)//10)
-            count+=1
-        return True if sum1==sum2 else False
+        even = odd = 0
+        for i in range(0,len(num),2):
+            even += int(num[i])
+        for j in range(1,len(num),2):
+            odd += int(num[j])
+        return even == odd
         
