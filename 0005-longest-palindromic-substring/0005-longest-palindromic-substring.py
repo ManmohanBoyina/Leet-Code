e66ec77f65fd69c87ce0f1a658a501b0938dba80
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2025-04-28 17:00:10 -0400

    Time: 254 ms (86.32%), Space: 17.8 MB (84.37%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,19 +1,18 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        start,end,maxi=0,0,0
-        for i in range(len(s)):
-            l1=self.getindex(s,i,i)
-            l2=self.getindex(s,i,i+1)
-            maxi=max(l1,l2)
-            if maxi>end-start:
-                start=i-(maxi-1)//2
-                end=i+maxi//2
-        return s[start:end+1]
-
-
+class Solution:
     def getindex(self,s,left,right):
         while left>=0 and right<len(s) and s[left]==s[right]:
             left-=1
             right+=1
         return right-left-1
+    def longestPalindrome(self, s: str) -> str:
+        start,end,maxi=0,0,0
+        for i in range(len(s)):
+            l1=self.getindex(s,i,i)
+            l2=self.getindex(s,i,i+1)
+            maxi=max(l1,l2)
+            if maxi>(end-start):
+                start=i-(maxi-1)//2
+                end=i+maxi//2
+        return s[start:end+1]
+        
         
