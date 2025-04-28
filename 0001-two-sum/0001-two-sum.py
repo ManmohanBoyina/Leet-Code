dd6a01c3701b4ffc609da80a082e84a98ce80a40
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2025-04-28 16:31:47 -0400

    Time: 0 ms (100%), Space: 19.1 MB (24.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        reference={}
+        a={}
         for i in range(len(nums)):
             ref=target-nums[i]
-            if ref in reference:
-                return [reference[ref],i]
-            reference[nums[i]]=i
-        return [-1,-1]
+            if ref in a:
+                return [a[ref],i]
+            a[nums[i]]=i
         
