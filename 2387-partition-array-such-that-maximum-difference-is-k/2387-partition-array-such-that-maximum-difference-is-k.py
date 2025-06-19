93b367d1fd39453782882b6a4a4cd234cfedd4a8
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2025-06-18 20:44:06 -0400

    Time: 118 ms (94.74%), Space: 21.7 MB (40.35%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.py
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.py
@@ -1,19 +1,10 @@
 class Solution(object):
     def partitionArray(self, nums, k):
-        res = []
+        ans=1
         nums.sort()
-        i = 0
-        while i < len(nums):
-            ans = []
-            j = i + 1
-            ans.append(nums[i])
-            while j != len(nums):
-                if nums[j] - nums[i] <= k:
-                    ans.append(nums[j])
-                    j += 1
-                else:
-                    break
-            res.append(ans[:])
-            i = j
-        print(res)
-        return len(res)
+        rec=nums[0]
+        for i in nums:
+            if i-rec>k:
+                ans+=1
+                rec=i
+        return ans
