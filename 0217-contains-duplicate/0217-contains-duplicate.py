8cd2e41343a046f03df7b1a23f3743433094f497
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2025-07-06 16:44:16 -0400

    Time: 15 ms (77.82%), Space: 24.5 MB (87.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,9 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        a={}
+class Solution(object):
+    def containsDuplicate(self, nums):
+        ans={}
         for i in nums:
-            if i in a:
+            if i in ans:
                 return True
-            else:
-                a[i]=1
+            ans[i]=1
         return False
         
