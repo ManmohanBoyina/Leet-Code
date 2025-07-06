5f0c8f4cc93d1d012c0fed41a534ae302083eae6
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2025-07-06 17:30:33 -0400

    Time: 0 ms (100%), Space: 14.1 MB (50.44%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def singleNumber(self, nums):
-        result = 0
+        result=0
         for num in nums:
-            result ^= num
+            result^=num
         return result
+        
