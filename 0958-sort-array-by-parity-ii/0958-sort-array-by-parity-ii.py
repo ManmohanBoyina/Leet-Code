3b710302b5fffe5bc8d46cd6a18348beb12da78c
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2025-10-31 14:49:02 -0400

    Time: 23 ms (16.52%), Space: 14.2 MB (96.01%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.py
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.py
@@ -4,6 +4,8 @@ class Solution(object):
         while even<len(nums) and odd<len(nums):
             if nums[even]%2==1 and nums[odd]%2==0:
                 nums[even],nums[odd]=nums[odd],nums[even]
+                even+=2
+                odd+=2
             elif nums[even]%2==0:
                 even+=2
             elif nums[odd]%2==1:
