7ad0e98be0eb6886c16f82ed1776341e655fc1fd
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2025-10-31 14:37:03 -0400

    Time: 0 ms (100%), Space: 12.4 MB (53.37%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.py
+++ 0861-flipping-an-image/0861-flipping-an-image.py
@@ -3,9 +3,5 @@ class Solution(object):
         for i in range(len(image)):
             for j in range(len(image[0])):
                 image[i][j]=image[i][j]^1
-            start,end=0,len(image[0])-1
-            while start<end:
-                image[i][start],image[i][end]=image[i][end],image[i][start]
-                start+=1
-                end-=1
+            image[i]=image[i][::-1]
         return image
