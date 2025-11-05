091a3e2e1d83906d537c185804cd475166b921cb
Manmohan Boyina <162081659+ManmohanBoyina@users.noreply.github.com>
2025-11-05 11:10:42 -0500

    Time: 18 ms (18.76%), Space: 14.8 MB (6.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,19 @@
 class Solution(object):
     def majorityElement(self, nums):
-        count={}
-        res,maxCount=0,0
+        count=0
+        num=nums[0]
         for i in range(len(nums)):
-            if nums[i] not in count:
-                count[nums[i]]=1
-            else:
-                count[nums[i]]+=1
-            if count[nums[i]]>maxCount:
-                maxCount=count[nums[i]]
-                res=nums[i]
-        return res
+            if count==0:
+                num=nums[i]
+            if num==nums[i]:
+                count+=1
+            elif num!=nums[i]:
+                count-=1
+        count=0
+        for i in range(len(nums)):
+            if nums[i]==num:
+                count+=1
+        if count>=len(nums)//2:
+            return num
 
         
